24f5292faf59967854b4b81c708195d7da9103e0
Ira <ibakumenko997@gmail.com>
2021-03-08 18:27:54 +0300

    refactor gh-pages

---
+++ build/precache-manifest.ea78e50abdfffab713f0a711d5be2773.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d02d9fe36b14b34d6a8",
+    "url": "/github-pages-react-app/static/css/main.aef2cd64.chunk.css"
+  },
+  {
+    "revision": "8d02d9fe36b14b34d6a8",
+    "url": "/github-pages-react-app/static/js/main.8d02d9fe.chunk.js"
+  },
+  {
+    "revision": "2bb80d608fc90ba8e309",
+    "url": "/github-pages-react-app/static/css/1.f5cb764f.chunk.css"
+  },
+  {
+    "revision": "2bb80d608fc90ba8e309",
+    "url": "/github-pages-react-app/static/js/1.2bb80d60.chunk.js"
+  },
+  {
+    "revision": "6681699cf880839a61ad",
+    "url": "/github-pages-react-app/static/js/runtime~main.6681699c.js"
+  },
+  {
+    "revision": "fe2f2a361d098a4d1b906e70e9c1f56c",
+    "url": "/github-pages-react-app/index.html"
+  }
+];

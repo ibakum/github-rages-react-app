345bdd2138165cefab4e910f53c5aad9c6a14215
Ira <ibakumenko997@gmail.com>
2021-03-12 19:48:02 +0300

    add: error

--- src/components/randomChar/randomChar.js
+++ src/components/randomChar/randomChar.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
 import './randomChar.css';
 import gotService from '../../services/gotService'; 
-import Spinner from '../spinner/';
+import Spinner from '../spinner';
+import ErrorMessage from '../errorMessage';
 
 export default class RandomChar extends Component {
     constructor() {
@@ -19,6 +20,14 @@ export default class RandomChar extends Component {
 onCharLoaded = (char) => {
     this.setState({
         char,
+        loading: false,
+        error: false
+    })
+}
+
+onError = (err) => {
+    this.setState({
+        error: true,
         loading: false
     })
 }
@@ -26,16 +35,21 @@ onCharLoaded = (char) => {
     updateChar() {
         const id = Math.floor(Math.random()*140 + 25);
         this.gotService.getCharacter(id)
-            .then(this.onCharLoaded);
+            .then(this.onCharLoaded)
+            .catch(this.onError);
     }
 
     render() {
-        const {char, loading} = this.state; 
+        const {char, loading, error} = this.state; 
 
-        const content = loading ? <Spinner/> : <View char={char}/>;
+        const errorMessage = error ? <ErrorMessage/> : null;
+        const spinner = loading ? <Spinner/> : null;
+        const content = !(loading || error) ? <View char={char}/> : null;
 
         return (
             <div className="random-block rounded">
+                {errorMessage}
+                {spinner}
                 {content}
             </div>
         );
